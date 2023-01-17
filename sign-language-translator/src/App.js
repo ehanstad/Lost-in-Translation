bd9034eec5705a5ce78286967b47575f1cd9378f
Erik <erik_hanstad@hotmail.com>
2023-01-17 14:06:32 +0100

    implement redux for the app

--- sign-language-translator/src/App.js
+++ sign-language-translator/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import store from './store.js';
+
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+        </header>
+      </div>
+    </Provider>
   );
 }
 
